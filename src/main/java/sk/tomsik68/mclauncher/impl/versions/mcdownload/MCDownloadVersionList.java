07bbdad493bf592f83abba4ce9016da8c5017973
tomsik68 <tomsik68@gmail.com>
2015-05-19 20:18:56 +0200

    Starting resolve dependencies in MCDownloadVersionList

--- src/main/java/sk/tomsik68/mclauncher/impl/versions/mcdownload/MCDownloadVersionList.java
+++ src/main/java/sk/tomsik68/mclauncher/impl/versions/mcdownload/MCDownloadVersionList.java
@@ -9,12 +9,18 @@ import sk.tomsik68.mclauncher.api.versions.IVersionList;
 import sk.tomsik68.mclauncher.impl.common.Observable;
 import sk.tomsik68.mclauncher.util.HttpUtils;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public final class MCDownloadVersionList extends Observable<IVersion> implements IVersionList {
     private static final String JSONVERSION_LIST_URL = "http://s3.amazonaws.com/Minecraft.Download/versions/versions.json";
     private static final String FULL_VERSION_URL_TEMPLATE = "http://s3.amazonaws.com/Minecraft.Download/versions/<VERSION>/<VERSION>.json";
+    private HashMap<String, MCDownloadVersion> versionMap = new HashMap<String, MCDownloadVersion>();
+    private HashSet<String> resolved = new HashSet<String>();
 
     @Override
     public void startDownload() throws Exception {
+        versionMap.clear();
         // at first, we download the complete version list
         String jsonString = HttpUtils.httpGet(JSONVERSION_LIST_URL);
         JSONObject versionInformation = (JSONObject) JSONValue.parse(jsonString);
@@ -27,8 +33,24 @@ public final class MCDownloadVersionList extends Observable<IVersion> implements
             JSONObject fullVersionObject = (JSONObject) JSONValue.parse(fullVersionJSONString);
             // ,create a MCDownloadVersion based on it
             MCDownloadVersion version = new MCDownloadVersion(fullVersionObject);
-            // and finally notify observers about a new version
-            notifyObservers(version);
+            // and save it inside the version map
+            versionMap.put(version.getId(), version);
         }
+
+        // resolve inheritsFrom in versions
+        for(MCDownloadVersion version : versionMap.values()){
+            // if version needs to get dependencies
+            if(version.getInheritsFrom() != null){
+                resolveDependencies(version);
+            }
+        }
+    }
+
+    private void resolveDependencies(MCDownloadVersion version){
+        // parent's parent needs to be resolved first
+        MCDownloadVersion parent = versionMap.get(version.getInheritsFrom());
+        if(parent.getInheritsFrom() != null)
+            resolveDependencies(parent);
+
     }
 }
