5a3ddbc06475b88d4529fde90bf44d20a3ea3951
tomsik68 <tomsik68@gmail.com>
2015-06-21 10:37:45 +0000

    MinecraftLauncherBackend can now list minecraft versions, install new versions and launch installed versions
    It's very useful if you don't want to mess up with low-level methods

---
+++ src/main/java/sk/tomsik68/mclauncher/backend/AddToListObserver.java
@@ -0,0 +1,22 @@
+package sk.tomsik68.mclauncher.backend;
+
+import sk.tomsik68.mclauncher.api.common.IObservable;
+import sk.tomsik68.mclauncher.api.common.IObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class AddToListObserver implements IObserver<String> {
+    private final List<String> list;
+    AddToListObserver(){
+        list = new ArrayList<>();
+    }
+    @Override
+    public void onUpdate(IObservable<String> observable, String changed) {
+        list.add(changed);
+    }
+
+    public List<String> getList(){
+        return list;
+    }
+}
