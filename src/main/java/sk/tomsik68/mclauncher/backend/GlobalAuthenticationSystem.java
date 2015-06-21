43773f7bc54d4bc518a50f3e5bf25111e2480c26
tomsik68 <tomsik68@gmail.com>
2015-06-21 09:33:46 +0000

    Added documentation for GlobalAuthenticationSystem

--- src/main/java/sk/tomsik68/mclauncher/backend/GlobalAuthenticationSystem.java
+++ src/main/java/sk/tomsik68/mclauncher/backend/GlobalAuthenticationSystem.java
@@ -8,8 +8,18 @@ import sk.tomsik68.mclauncher.impl.login.yggdrasil.YDProfileIO;
 
 import java.io.File;
 
+/**
+ * Global(system-wide) authentication system.
+ * It loads authentication information from .minecraft directory and updates them after login,
+ * so you'll never need to enter your password into a custom launcher.
+ */
 public final class GlobalAuthenticationSystem {
 
+    /**
+     * Obtains list of profiles <b>in .minecraft directory</b>
+     * @return List of names of profiles in .minecraft directory
+     * @throws Exception - File I/O errors, JSON parsing errors
+     */
     public static String[] getProfileNames() throws Exception {
         File workingDirectory = Platform.getCurrentPlatform().getWorkingDirectory();
         YDProfileIO profileIO = new YDProfileIO(workingDirectory);
@@ -21,6 +31,12 @@ public final class GlobalAuthenticationSystem {
         return profileNames;
     }
 
+    /**
+     * Tries to perform login using profile with specified name
+     * @param name Name of profile you want to use. If there are
+     * @return ISession which is necessary to play the game
+     * @throws Exception File I/O, JSON parsing, Network I/O, Profile Selection errors
+     */
     public static ISession login(String name) throws Exception {
         // initialise login service in the working directory
         File workingDirectory = Platform.getCurrentPlatform().getWorkingDirectory();
@@ -30,6 +46,8 @@ public final class GlobalAuthenticationSystem {
         // read profiles
         YDProfileIO profileIO = new YDProfileIO(workingDirectory);
         IProfile[] profiles = profileIO.read();
+        if(profiles.length == 0)
+            throw new ProfileSelectionException("There are no profiles in .minecraft directory!");
         // select first profile.
         IProfile selectedProfile = profiles[0];
         // if there are more profiles and name is not empty
