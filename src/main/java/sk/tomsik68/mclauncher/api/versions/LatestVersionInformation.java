7f34cf4f9346f50e64dd18627428acd9cd5601fc
tomsik68 <tomsik68@gmail.com>
2015-06-21 14:58:55 +0000

    LatestVersionInformation will store only latestRelease and latestSnapshot

--- src/main/java/sk/tomsik68/mclauncher/api/versions/LatestVersionInformation.java
+++ src/main/java/sk/tomsik68/mclauncher/api/versions/LatestVersionInformation.java
@@ -1,7 +1,18 @@
 package sk.tomsik68.mclauncher.api.versions;
 
-public class LatestVersionInformation {
-    public LatestVersionInformation(){
+public final class LatestVersionInformation {
+    private final String latestRelease;
+    private final String latestSnapshot;
+    public LatestVersionInformation(String lastRelease, String lastSnapshot){
+        this.latestRelease = lastRelease;
+        this.latestSnapshot = lastSnapshot;
+    }
 
+    public String getLatestRelease() {
+        return latestRelease;
+    }
+
+    public String getLatestSnapshot() {
+        return latestSnapshot;
     }
 }
