841e65c93a7fe900c955c5647e6613a3d4f306ba
tomsik68 <tomsik68@gmail.com>
2015-06-21 08:54:14 +0000

    Updated TestLogin file to use the utility class

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -2,29 +2,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import sk.tomsik68.mclauncher.api.common.MCLauncherAPI;
 import sk.tomsik68.mclauncher.api.login.IProfile;
 import sk.tomsik68.mclauncher.api.login.ISession;
 import sk.tomsik68.mclauncher.impl.common.Platform;
 import sk.tomsik68.mclauncher.impl.login.yggdrasil.YDLoginService;
 import sk.tomsik68.mclauncher.impl.login.yggdrasil.YDProfileIO;
+import sk.tomsik68.mclauncher.util.auth.MinecraftAuthentication;
 
 public class TestLogin {
 
     @Test
-    public void test() throws Exception {
-        YDLoginService yls = new YDLoginService();
-        yls.load(Platform.getCurrentPlatform().getWorkingDirectory());
-            
-        YDProfileIO profileIO = new YDProfileIO(Platform.getCurrentPlatform().getWorkingDirectory());
-        IProfile[] profiles = profileIO.read();
-        IProfile profile = profiles[0];
-            
-        ISession session;
-        session = yls.login(profile);
-        assertNotNull(session);
-            
-        profiles[0] = profile;
-        profileIO.write(profiles);
+    public void test() {
+        try {
+            ISession session = MinecraftAuthentication.login(null);
+            MCLauncherAPI.log.info("Login successful!");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
