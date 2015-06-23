15403eb9a744fcb66bbf78ae52224bcf7ef453d0
tomsik68 <tomsik68@gmail.com>
2015-06-23 10:49:00 +0000

    Added docs to LatestVersionInformation

--- src/main/java/sk/tomsik68/mclauncher/api/versions/LatestVersionInformation.java
+++ src/main/java/sk/tomsik68/mclauncher/api/versions/LatestVersionInformation.java
@@ -1,5 +1,8 @@
 package sk.tomsik68.mclauncher.api.versions;
 
+/**
+ * Contains information about latest versions in a given version list
+ */
 public final class LatestVersionInformation {
     private final String latestRelease;
     private final String latestSnapshot;
@@ -8,10 +11,18 @@ public final class LatestVersionInformation {
         this.latestSnapshot = lastSnapshot;
     }
 
+    /**
+     *
+     * @return ID of latest release version. E.g. "1.8"
+     */
     public String getLatestRelease() {
         return latestRelease;
     }
 
+    /**
+     *
+     * @return ID of latest snapshot. E.g. "14w07a"
+     */
     public String getLatestSnapshot() {
         return latestSnapshot;
     }
